e8cceb207a8e0e93f65a9b9f0f58ccc2ebee035d
Lorenzo Gentile <lorenzo.gentile2001@hotmail.it>
2025-05-20 14:34:41 +0200

    forgot one file.

--- src/NDAnalysis2D.cpp
+++ src/NDAnalysis2D.cpp
@@ -16,7 +16,7 @@ static NDConfig config_square = {
     .degree = 1,
     .d_ext = 0.00,
     .d_axn = 0.2,
-    .C_0 = 0.4,
+    .C_0 = 0.9,
     // .mesh = "../meshes/mesh-square-40.msh",
     .mesh = "../meshes/mesh-square-200.msh",
 };
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
 
   const Point<2> random_point(0.7, 0.7);
   ConstantInitialCondition<2> initial_condition(config.C_0, random_point, 0.1);
-  AxonBasedFiberField<2> fiber_field(0.3, square_origin);
+  AxonBasedFiberField<2> fiber_field(square_origin, Point<2>(0.3, 0.2));
   //RadialFiberField<2> fiber_field(square_origin);
   //CircumferentialFiberField<2> fiber_field(square_origin);
 
